d9f808775ec7699eb6e59054b13ae8d71ec13523
Noemi Ashizuka <noemi.calanducci@gmail.com>
2020-05-08 16:46:58 +0900

    about page basic styling

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
 
+import '../assets/stylesheets/about.scss';
+
 class About extends Component {
   render() {
     return(
-      <div><h1>About Me</h1></div>
+      <div className="page-container">
+        <h1 className="page-title">About Me</h1>
+      <div className="content-container">
+        <div className="content-image">
+
+        </div>
+        <div className="content-text">
+
+        </div>
+      </div>
+    </div>
     )
   }
 }
