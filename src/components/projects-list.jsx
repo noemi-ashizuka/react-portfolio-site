5a1f732cc94192a83875fd2b841c1c47b30b0728
Noemi Ashizuka <noemi.calanducci@gmail.com>
2020-05-08 18:07:10 +0900

    added basic projects data

--- src/components/projects-list.jsx
+++ src/components/projects-list.jsx
@@ -3,7 +3,15 @@ import React, { Component } from 'react';
 class ProjectsList extends Component {
   render() {
     return(
-      <div><h1>All my Projects</h1></div>
+      <div>
+        <h1>All my Projects</h1>
+        <div className="project-list">
+          <div className="project-card">
+            
+          </div>
+        </div>
+      
+      </div>
     )
   }
 }
---
+++ src/data/projects-data.js
@@ -0,0 +1,94 @@
+const PROJECTS_DATA = [
+  {
+    id: 1,
+    title: "Clever Closet",
+    titleImg: "",
+    imagesUrls: [],
+    specifications: "Group project | Role: team manager | Time: 2 weeks",
+    description: "Clever Closet is an app to manage your wardrobe and help reduce the impact of fast fashion on the environment. How does it do that? Simple, with Clever Clothes you can share clothes with friends, create looks for yourself and your friends to make the most of the clothes you already own. In this project I was responsible for my team workflow, and I coded for the backend as well as the frontend.",
+    links: [
+      {github: ""},
+      {siteUrl: ""},
+      {videoUrl: ""}
+      ],
+      technologies: [
+        {name: "Ruby", iconUrl: ""},
+        {name: "Rails", iconUrl: ""},
+        {name: "HTML", iconUrl: ""},
+        {name: "SCSS", iconUrl: ""},
+        {name: "JavaScript", iconUrl: ""},
+        {name: "PostgreSQL", iconUrl: ""},
+        {name: "Figma", iconUrl: ""},
+        {name: "Trello", iconUrl: ""}
+      ]
+  },
+  {
+    id: 2,
+    title: "Obi",
+    titleImg: "",
+    imagesUrls: [],
+    specifications: "Group project | Role: team manager | Time: 1 weeks",
+    description: "Obi is a marketplace where people can rent or lend kimonos of many different types.For this project I was responsible for database design, assigning tasks to the team and coding for the backend as well as frontend.",
+    links: [
+      {github: ""},
+      {siteUrl: ""},
+      {videoUrl: ""}
+      ],
+    technologies: [
+      {name: "Ruby", iconUrl: ""},
+      {name: "Rails", iconUrl: ""},
+      {name: "HTML", iconUrl: ""},
+      {name: "SCSS", iconUrl: ""},
+      {name: "JavaScript", iconUrl: ""},
+      {name: "PostgreSQL", iconUrl: ""},
+      {name: "Figma", iconUrl: ""},
+      {name: "Trello", iconUrl: ""}
+    ]
+  },
+  {
+    id: 3,
+    title: "Bartender Cocktail",
+    titleImg: "",
+    imagesUrls: [],
+    description: "Project for Le Wagon bootcamp. The purpose was to learn how to develop an app with complex databases relations.",
+    specifications: "Solo project | Time: 2 days",
+    links: [
+      {github: ""},
+      {siteUrl: ""},
+      {videoUrl: ""}
+      ],
+    technologies: [
+      {name: "Ruby", iconUrl: ""},
+      {name: "Rails", iconUrl: ""},
+      {name: "HTML", iconUrl: ""},
+      {name: "SCSS", iconUrl: ""},
+      {name: "JavaScript", iconUrl: ""},
+      {name: "PostgreSQL", iconUrl: ""},
+      {name: "Figma", iconUrl: ""},
+      {name: "Trello", iconUrl: ""}
+    ]
+  },
+  {
+    id: 4,
+    title: "Sailor Maker",
+    titleImg: "",
+    imagesUrls: [],
+    description: "Le Wagon weekend challenge. I decided to go for something I am passionate about and created an app where people can upload their own Sailor Scouts and choose powers for them.",
+    specifications: "Solo project | Time: 2 days",
+    links: [
+      {github: ""},
+      {siteUrl: ""},
+      {videoUrl: ""}
+      ],
+    technologies: [
+      {name: "Ruby", iconUrl: ""},
+      {name: "Rails", iconUrl: ""},
+      {name: "HTML", iconUrl: ""},
+      {name: "SCSS", iconUrl: ""},
+      {name: "JavaScript", iconUrl: ""},
+      {name: "PostgreSQL", iconUrl: ""},
+      {name: "Figma", iconUrl: ""},
+      {name: "Trello", iconUrl: ""}
+    ]
+  }
+]
