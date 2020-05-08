ee7f037950b823931e7ee3081337455d8c538b31
Noemi Ashizuka <noemi.calanducci@gmail.com>
2020-05-08 17:10:04 +0900

    added footer component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Main from './components/main';
 import Header from './components/header';
+import Footer from './components/footer';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
   <div>
     <Header />
     <Main />
+    <Footer />
   </div>
   );
 }
---
+++ src/components/footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import '../assets/stylesheets/footer.scss';
+
+const Footer = () => (
+  <div className="footer">
+    <div className="footer-links">
+      <a href="/" className="f-link">I'm a link</a>
+      <a href="/" className="f-link">I'm a link</a>
+      <a href="/" className="f-link">I'm a link</a>
+    </div>
+  </div>
+)
+
+export default Footer;
