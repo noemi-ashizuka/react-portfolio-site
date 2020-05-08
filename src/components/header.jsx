818ec5055c2b961ffb720bae82c465a69a02c6da
Noemi Ashizuka <noemi.calanducci@gmail.com>
2020-05-08 22:37:32 +0900

    made navbar responsive

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,23 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-const Header = () => (
-  <div className="header-bar">
-    <Link className="logo" to="/"></Link>
-    <div className="header-links">
-      <Link className="link" to="/about">
-        About
-      </Link>
-      <span className="divisor">/</span>
-      <Link className="link" to="/projects">
-        Projects
-      </Link>
-      <span className="divisor">/</span>
-      <Link className="link" to="/contact">
-        Contact
-      </Link>
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isExpanded: false
+    };
+  }
+
+  handleToggle(e) {
+    e.preventDefault();
+    this.setState({
+      isExpanded: !this.state.isExpanded
+    });
+  }
+
+  render() {
+    const { isExpanded } = this.state;
+    return(
+    <div className="header-bar">
+      <Link className="logo" to="/">Logo</Link>
+      <nav className="nav">
+        <i
+          className="fas fa-bars"
+          aria-hidden="true"
+          onClick={e => this.handleToggle(e)}
+        />
+        
+        <div className="header-links">
+          <ul className={`collapsed ${isExpanded ? "is-expanded" : ""}`}>
+            <Link className="link" to="/about">
+              About
+            </Link>
+            <span className="divisor">/</span>
+            <Link className="link" to="/projects">
+              Projects
+            </Link>
+            <span className="divisor">/</span>
+            <Link className="link" to="/contact">
+              Contact
+            </Link>
+          </ul>
+        </div>
+        
+      </nav>
+      
     </div>
-  </div>
-)
+    )
+  }
+  
+}
 
 export default Header;
