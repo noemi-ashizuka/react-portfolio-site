e696e15116498087bae297aeba9ee3d400596a57
Noemi Ashizuka <noemi.calanducci@gmail.com>
2020-05-11 11:55:24 +0900

    styled buttons

--- src/components/project-details.jsx
+++ src/components/project-details.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+
+import '../assets/stylesheets/project-details.scss';
 import PROJECTS_DATA from '../data/projects-data';
 
 
@@ -10,13 +12,19 @@ class ProjectDetails extends Component {
   render() {
     let project = PROJECTS_DATA.find(element => element.id === parseInt(this.props.match.params.projectId))
     return(
-      <div>
-        <h1>{project.title}</h1>
-        <img src={project.imagesUrls}></img>
-        <h2>{project.specifications}</h2>
-        <p>{project.description}</p>
-        <p>{project.technologies.map((tech) =>( `${tech.name} ` ))}</p>
-        <p>{project.links.map((link) => (`${link.key}`))}</p>
+      <div className="page-container">
+        <h1 className="project-title">{project.title}</h1>
+        <div className="project-images">
+          <img src={project.imagesUrls}></img>
+        </div>
+        <div className="project-description">
+          <h2>{project.specifications}</h2>
+          <p>{project.description}</p>
+          <p>{project.technologies.map((tech) =>( `${tech.name} ` ))}</p>
+        </div>
+        <div className="project-links">
+          {project.links.map((link) => (<a href="/" className="button-main">{link.github}</a>))}
+        </div>
       </div>
     )
   }
--- src/data/projects-data.js
+++ src/data/projects-data.js
@@ -7,9 +7,10 @@ const PROJECTS_DATA = [
     specifications: "Group project | Role: team manager | Time: 2 weeks",
     description: "Clever Closet is an app to manage your wardrobe and help reduce the impact of fast fashion on the environment. How does it do that? Simple, with Clever Clothes you can share clothes with friends, create looks for yourself and your friends to make the most of the clothes you already own. In this project I was responsible for my team workflow, and I coded for the backend as well as the frontend.",
     links: [
-      {github: ""},
-      {siteUrl: ""},
-      {videoUrl: ""}
+      {
+      github: "github",
+      siteUrl: "site",
+      videoUrl: "video"},
       ],
       technologies: [
         {name: "Ruby", iconUrl: ""},
@@ -30,9 +31,10 @@ const PROJECTS_DATA = [
     specifications: "Group project | Role: team manager | Time: 1 weeks",
     description: "Obi is a marketplace where people can rent or lend kimonos of many different types.For this project I was responsible for database design, assigning tasks to the team and coding for the backend as well as frontend.",
     links: [
-      {github: ""},
-      {siteUrl: ""},
-      {videoUrl: ""}
+      {
+      github: "github",
+      siteUrl: "site",
+      videoUrl: "video"},
       ],
     technologies: [
       {name: "Ruby", iconUrl: ""},
@@ -53,9 +55,10 @@ const PROJECTS_DATA = [
     description: "Project for Le Wagon bootcamp. The purpose was to learn how to develop an app with complex databases relations.",
     specifications: "Solo project | Time: 2 days",
     links: [
-      {github: ""},
-      {siteUrl: ""},
-      {videoUrl: ""}
+      {
+      github: "github",
+      siteUrl: "site",
+      videoUrl: "video"},
       ],
     technologies: [
       {name: "Ruby", iconUrl: ""},
@@ -76,9 +79,10 @@ const PROJECTS_DATA = [
     description: "Le Wagon weekend challenge. I decided to go for something I am passionate about and created an app where people can upload their own Sailor Scouts and choose powers for them.",
     specifications: "Solo project | Time: 2 days",
     links: [
-      {github: ""},
-      {siteUrl: ""},
-      {videoUrl: ""}
+      {
+      github: "github",
+      siteUrl: "site",
+      videoUrl: "video"},
       ],
     technologies: [
       {name: "Ruby", iconUrl: ""},
