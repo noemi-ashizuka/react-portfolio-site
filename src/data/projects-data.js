34b4be5bf1796510fb25616c8d833d88f7b036fc
Noemi Ashizuka <noemi.calanducci@gmail.com>
2020-05-15 16:11:39 +0900

    added project

--- src/data/projects-data.js
+++ src/data/projects-data.js
@@ -33,6 +33,36 @@ const PROJECTS_DATA = [
   },
   {
     id: 2,
+    title: "Tokyo Best Coffee Shops",
+    titleImg: "https://res.cloudinary.com/dtewjoisz/image/upload/v1589448628/projects/projects-slides/coffee_home_ocr3bh.png",
+    imagesUrls: ["https://res.cloudinary.com/dtewjoisz/image/upload/v1589448628/projects/projects-slides/coffee_home_ocr3bh.png", "https://res.cloudinary.com/dtewjoisz/image/upload/v1589448630/projects/projects-slides/coffee_show_almto0.png", "https://res.cloudinary.com/dtewjoisz/image/upload/v1589448633/projects/projects-slides/coffee_list_jykunx.png", "https://res.cloudinary.com/dtewjoisz/image/upload/v1589448634/projects/projects-slides/coffee_api_dqt7yg.png"],
+    specifications: "Solo project",
+    description: "An app to create articles about the best coffee shops in Tokyo. It includes reviews and menus and any user can contribute and help others find hidden gems. It also includes an API with all the users coffee shops data.",
+    links: [
+      {
+        id: 1,
+        url: "github"
+      },
+      {
+        id: 2,
+        url: "site"
+      },
+      {
+        id: 3,
+        url: "video"
+      }
+    ],
+    technologies: [
+      {name: "Ruby", iconUrl: ""},
+      {name: "Rails", iconUrl: ""},
+      {name: "HTML", iconUrl: ""},
+      {name: "SCSS", iconUrl: ""},
+      {name: "JavaScript", iconUrl: ""},
+      {name: "PostgreSQL", iconUrl: ""}
+    ]
+  },
+  {
+    id: 3,
     title: "Obi",
     titleImg: "https://res.cloudinary.com/dtewjoisz/image/upload/v1589181638/projects/Screen_Shot_2020-03-18_at_20.03.50_x8g6kt.png",
     imagesUrls: ["https://res.cloudinary.com/dtewjoisz/image/upload/v1589448667/projects/projects-slides/obi_home_hu9u8r.png","https://res.cloudinary.com/dtewjoisz/image/upload/v1589448652/projects/projects-slides/obi_show_kpymul.png","https://res.cloudinary.com/dtewjoisz/image/upload/v1589448647/projects/projects-slides/obi_dashnoe_u1jral.png", "https://res.cloudinary.com/dtewjoisz/image/upload/v1589448650/projects/projects-slides/obi_listings_qzkawa.png"],
@@ -58,16 +88,14 @@ const PROJECTS_DATA = [
       {name: "HTML", iconUrl: ""},
       {name: "SCSS", iconUrl: ""},
       {name: "JavaScript", iconUrl: ""},
-      {name: "PostgreSQL", iconUrl: ""},
-      {name: "Figma", iconUrl: ""},
-      {name: "Trello", iconUrl: ""}
+      {name: "SQLite", iconUrl: ""}
     ]
   },
   {
-    id: 3,
+    id: 4,
     title: "Bartender Cocktail",
     titleImg: "https://res.cloudinary.com/dtewjoisz/image/upload/v1589181624/projects/bc_home_ufoydh.png",
-    imagesUrls: ["https://res.cloudinary.com/dtewjoisz/image/upload/v1589181624/projects/bc_home_ufoydh.png"],
+    imagesUrls: ["https://res.cloudinary.com/dtewjoisz/image/upload/v1589181624/projects/bc_home_ufoydh.png", "https://res.cloudinary.com/dtewjoisz/image/upload/v1589448643/projects/projects-slides/bc_list_orpcm0.png", "https://res.cloudinary.com/dtewjoisz/image/upload/v1589448640/projects/projects-slides/bc_card_two_iieh4i.png", "https://res.cloudinary.com/dtewjoisz/image/upload/v1589448636/projects/projects-slides/bc_card_one_gffplt.png"],
     description: "Project for Le Wagon bootcamp. The purpose was to learn how to develop an app with complex databases relations.",
     specifications: "Solo project | Time: 2 days",
     links: [
@@ -90,16 +118,14 @@ const PROJECTS_DATA = [
       {name: "HTML", iconUrl: ""},
       {name: "SCSS", iconUrl: ""},
       {name: "JavaScript", iconUrl: ""},
-      {name: "PostgreSQL", iconUrl: ""},
-      {name: "Figma", iconUrl: ""},
-      {name: "Trello", iconUrl: ""}
+      {name: "SQLite", iconUrl: ""}
     ]
   },
   {
-    id: 4,
+    id: 5,
     title: "Sailor Maker",
     titleImg: "https://res.cloudinary.com/dtewjoisz/image/upload/v1589181647/projects/swm_home_hq02br.png",
-    imagesUrls: ["https://res.cloudinary.com/dtewjoisz/image/upload/v1589181647/projects/swm_home_hq02br.png"],
+    imagesUrls: ["https://res.cloudinary.com/dtewjoisz/image/upload/v1589181647/projects/swm_home_hq02br.png", "https://res.cloudinary.com/dtewjoisz/image/upload/v1589448669/projects/projects-slides/swm_card_x8oilh.png", "https://res.cloudinary.com/dtewjoisz/image/upload/v1589448676/projects/projects-slides/swm_list_shho5v.png", "https://res.cloudinary.com/dtewjoisz/image/upload/v1589448674/projects/projects-slides/swm_card_two_kkgilh.png"],
     description: "Le Wagon weekend challenge. I decided to go for something I am passionate about and created an app where people can upload their own Sailor Scouts and choose powers for them.",
     specifications: "Solo project | Time: 2 days",
     links: [
