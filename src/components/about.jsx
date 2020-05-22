af5658dae0ddae741dd431a1a20db6a80258d200
Noemi Ashizuka <noemi.calanducci@gmail.com>
2020-05-22 18:03:33 +0900

    added data to about page

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,21 +1,32 @@
 import React, { Component } from 'react';
 
+import ABOUT_DATA from '../data/about-data';
+
 import '../assets/stylesheets/about.scss';
 
 class About extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      collections: ABOUT_DATA
+    }
+  }
+
   render() {
+    const {collections} = this.state;
     return(
       <div className="page-container">
         <h1 className="page-title">About Me</h1>
-      <div className="content-container">
-        <div className="content-image">
-
-        </div>
-        <div className="content-text">
-
-        </div>
+          {
+            collections.map(({ imageUrl, description, ...otherCollectionProps }) => (
+              <div className="content-container">
+                <div className="content-image">{ imageUrl }</div>
+                <div className="content-text">{ description }</div>
+              </div>
+            ))
+          }
       </div>
-    </div>
     )
   }
 }
