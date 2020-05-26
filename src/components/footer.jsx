34ab25773a911c7be3efb2f0c7e68f14e52c3116
Noemi Ashizuka <noemi.calanducci@gmail.com>
2020-05-26 19:08:51 +0900

    fixed link in footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import '../assets/stylesheets/footer.scss';
 
@@ -7,7 +8,7 @@ const Footer = () => (
     <div className="footer-links">
       <a target="_blank" rel="noopener noreferrer" href="https://github.com/noemi-ashizuka" className="f-link"><i className="fab fa-github"></i></a>
       <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/salvina-noemi-calanducci/" className="f-link"><i className="fab fa-linkedin-in"></i></a>
-      <a target="_blank" rel="noopener noreferrer" href="/" className="f-link"><i className="fas fa-envelope"></i></a>
+      <Link className="f-link" to="/contact"><i className="fas fa-envelope"></i></Link>
     </div>
   </div>
 )
