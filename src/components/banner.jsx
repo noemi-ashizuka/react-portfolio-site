6affcfda6b67ba09a0c2ed91ed08a16eb0c9d643
Noemi Ashizuka <noemi.calanducci@gmail.com>
2020-05-26 10:43:34 +0900

    added banner component

---
+++ src/components/banner.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import '../assets/stylesheets/banner.scss';
+
+const Banner = () => (
+  <div className="banner">
+    <div className="banner-text">
+      <h1>HomePage</h1>
+      <p>This is a short description</p>
+    </div>
+  </div>
+)
+
+export default Banner;
--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 
+import Banner from './banner';
+
 class Home extends Component {
   render() {
     return(
-      <div><h1>Home Page</h1></div>
+      <Banner />
     )
   }
 }
