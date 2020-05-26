ec79808e70f1bf9bbcc6cb0d6383b2594c378a1e
Noemi Ashizuka <noemi.calanducci@gmail.com>
2020-05-26 17:53:55 +0900

    added socials to contact page

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -4,20 +4,19 @@ import MyForm from './my-form';
 
 import '../assets/stylesheets/contact-page.scss';
 
-class Contact extends Component {
-  render() {
-    return(
-      <div>
-        <div className="page-title">
-          <h1>Contact Me</h1>
-        </div>
-        <div className="social">
-
-        </div>
-        <MyForm />
+const Contact = () => {
+  return(
+    <div className="page-container">
+      <div className="page-title">
+        <h1>Contact Me</h1>
       </div>
-    )
-  }
+      <div className="socials">
+        <a target="_blank" rel="noopener noreferrer" href="https://github.com/noemi-ashizuka" className="contact-link"><i className="fab fa-github"></i></a>
+        <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/salvina-noemi-calanducci/" className="contact-link"><i className="fab fa-linkedin-in"></i></a>
+      </div>
+      <MyForm />
+    </div>
+  )
 }
 
 export default Contact;
